705808d668868035b120da6945568e01d04556b9
Albert <me@albert.sh>
2018-03-28 03:24:07 +0200

    add classification script and the datasets that resulted from running it

---
+++ classify.py
@@ -0,0 +1,19 @@
+import requests
+import json
+import pickle
+
+api_endpoint="http://text-processing.com/api/sentiment/" #API Documentation: http://text-processing.com/docs/sentiment.html
+analyzed=[]
+
+def hateIndex(o):
+    return o["hate"]
+
+for line in open("rants.txt", 'r'):
+    line=line[:-1] #Remove line break
+    result=requests.post(api_endpoint, data={"text":line})
+    negativity=json.loads(result.text)["probability"]["neg"]
+    analyzed.append({"text":line, "hate":negativity})
+analyzed.sort(key=hateIndex)
+open("data.json", 'w').write(json.dumps(analyzed))
+open("data.pkl", 'w').write(pickle.dumps(analyzed))
+
